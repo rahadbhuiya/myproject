fed741d37ee42fa1389d3072d47ca97caa3c5d1f
rahadbhuiya <rahadbhuiya001@gmail.com>
2025-05-27 08:16:47 -0400

    Initial commit

--- database/migrations/2025_05_10_201737_create_games_table.php
+++ database/migrations/2025_05_10_201737_create_games_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
-            $table->string('name'); // গেমের নাম
-            $table->string('logo'); // গেমের লোগো
-            $table->text('description'); // গেমের বর্ণনা
-            $table->foreignId('category_id')->constrained()->onDelete('cascade'); // গেমের ক্যাটেগরি
-            $table->timestamps();
+            $table->string('name'); 
+            $table->string('logo'); 
+            $table->text('description'); 
+            $table->foreignId('category_id')->constrained()->onDelete('cascade'); 
         });
     }
 
